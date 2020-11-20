d5b2df11be2a9bd5f367b142959e286ecefaccab
sahariarprogrammer <sahariar55@gmail.com>
2020-11-20 12:36:54 +0600

    Updates

---
+++ precache-manifest.3c150e65fee1fbe070d58f74f0dc5e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c0fa786dc09e58fa7351a2010eeb83",
+    "url": "/it-world/index.html"
+  },
+  {
+    "revision": "60027dd8dd73b4d81cb8",
+    "url": "/it-world/static/css/main.263a8d68.chunk.css"
+  },
+  {
+    "revision": "1514c134ffcec4456292",
+    "url": "/it-world/static/js/2.62133f24.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/it-world/static/js/2.62133f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60027dd8dd73b4d81cb8",
+    "url": "/it-world/static/js/main.e410cbd0.chunk.js"
+  },
+  {
+    "revision": "ab4898eca17f9c881de8",
+    "url": "/it-world/static/js/runtime-main.4a9a712d.js"
+  }
+]);
